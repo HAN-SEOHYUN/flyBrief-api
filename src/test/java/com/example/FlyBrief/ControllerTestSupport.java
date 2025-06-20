784d6098722b3a0b05e3848f200b85e73991cea3
HANSEOHYUN <000312@daum.net>
2025-06-20 23:46:42 +0900

    refactor: JPA 미사용에 따른 설정 제거

--- src/test/java/com/example/FlyBrief/ControllerTestSupport.java
+++ src/test/java/com/example/FlyBrief/ControllerTestSupport.java
@@ -4,7 +4,6 @@ import com.example.FlyBrief.api.controller.test.TestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(controllers = {
