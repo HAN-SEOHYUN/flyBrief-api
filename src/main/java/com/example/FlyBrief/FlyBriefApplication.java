05023d83c90d6ed2fd1b28c70676a1759ce77507
HANSEOHYUN <000312@daum.net>
2025-06-20 18:15:36 +0900

    Initial commit for FlyBrief project

---
+++ src/main/java/com/example/FlyBrief/FlyBriefApplication.java
@@ -0,0 +1,14 @@
+package com.example.FlyBrief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyBriefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyBriefApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
