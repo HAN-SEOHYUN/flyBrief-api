99fd608e1f92a5aa68e4e19765de8e732259e582
HANSEOHYUN <000312@daum.net>
2025-06-25 18:27:35 +0900

    [task] 날씨 요약 API 호출 기능 구현 resolve #20

--- src/main/java/com/example/FlyBrief/api/controller/weather/WeatherController.java
+++ src/main/java/com/example/FlyBrief/api/controller/weather/WeatherController.java
@@ -2,6 +2,7 @@ package com.example.FlyBrief.api.controller.weather;
 
 import com.example.FlyBrief.api.ApiResponse;
 import com.example.FlyBrief.api.service.weather.WeatherService;
+import com.example.FlyBrief.api.service.weather.WeatherSummaryService;
 import com.example.FlyBrief.api.service.weather.response.WeatherResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/weather")
 public class WeatherController {
     private final WeatherService weatherService;
+    private final WeatherSummaryService weatherSummaryService;
 
-    public WeatherController(WeatherService weatherService) {
+    public WeatherController(WeatherService weatherService, WeatherSummaryService weatherSummaryService) {
         this.weatherService = weatherService;
+        this.weatherSummaryService = weatherSummaryService;
     }
 
     @GetMapping("/forecast")
@@ -27,5 +31,15 @@ public class WeatherController {
     ) {
         List<WeatherResponse> results = weatherService.getWeatherForecast(location, startDate, endDate);
         return ApiResponse.ok(results);
+    }
+
+    @GetMapping("/summary")
+    public Map<String, String> getWeatherSummary(
+            @RequestParam String location,
+            @RequestParam String startDate,
+            @RequestParam String endDate
+    ) {
+        String message = weatherSummaryService.fetchWeatherSummary(location, startDate, endDate);
+        return Map.of("message", message);
     }
 }
